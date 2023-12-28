a1c99a60f2978a1c5bee9ba21c7d2cb71a26056b
annapurnachinta <annapurnachinta1234@gmail.com>
2023-12-28 18:25:34 +0530

    learned about express, path, html, hbs

---
+++ Section 7 - Web Servers (Weather App)/src/app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// setup handlebars for engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+
+// setup static director to serve
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        footer: 'Annapurna'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather',
+        footer: 'Annapurna'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        title: 'Help App',
+    })
+})
+
+app.get('*', (req,res) => {
+    res.render('404-error', {
+        title: 'page not found',
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+})
