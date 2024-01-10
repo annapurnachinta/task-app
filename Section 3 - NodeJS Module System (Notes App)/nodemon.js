d37ca3bb465b7649b614ea94be8e164df7c26d81
annapurnachinta <36999742+annapurnachinta@users.noreply.github.com>
2024-01-10 22:14:51 +0530

    Delete Section 3 - NodeJS Module System (Notes App) directory

--- Section 3 - NodeJS Module System (Notes App)/nodemon.js
+++
@@ -1,6 +0,0 @@
-// npm install nodemon -g
-// nodemon -v
-//  if we getting error in version then below is the solution
-// click windows and open powershell as run as administration
-// run set-ExecutionPolicy Unrestricted
-// Rerun nodemon -v 
